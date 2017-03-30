ca5ced277146aca9d5f02e9919d5c3bebb261581
MTZimmer <zimmermann.michael@mayo.edu>
2017-03-30 12:19:06 -0500

    added cache of coding genes
    cached a list of coding genes to speed up vignettes

---
+++ R/cached_coding_genes.R
@@ -0,0 +1,9 @@
+#' @title cached_coding_genes.RData
+#' @description  gene symbols for protein coding genes - cached for use in vignettes - 
+#' @format A \code{chr} vector of gene symbols for all protein coding genes
+#' @source ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/locus_groups/protein-coding_gene.txt
+#' @return Chr vector of gene symbols
+#' @author Michael T Zimmermann
+#' @examples
+#' print(str(cached_coding_genes))
+"cached_coding_genes"
