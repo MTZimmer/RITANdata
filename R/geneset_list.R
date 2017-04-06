4bffd866a776c09fdcfd45bc4776b0a4c866d142
Michael Zimmermann <zimmermann.michael@mayo.edu>
2017-04-06 10:13:05 -0500

    updated documentation

--- R/geneset_list.R
+++ R/geneset_list.R
@@ -1,6 +1,6 @@
 #' @title geneset_list.RData
-#' @description R list object containing 15 annotation resources and the gene sets contained in each of them.
-#' @format A \code{list} with 15 elements, one for each annotation warehouse. Each element contains a varying number of vectors of gene symbols.
+#' @description R list object containing annotation resources and the gene sets contained in each of them. 
+#' @format A \code{list} with one element for each annotation resource. Each element contains a varying number of gene symbols.
 #' @source Multiple sources used. See our table in the network vignette.
 #' @return A list of resources. Each resource contains a list of genesets.
 #' @author Michael T Zimmermann
